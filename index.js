0d87b6abafe2044ec3c5e8169734e1a09be11a59
Dartrix <darwinramos2009@hotmail.com>
2019-02-21 13:38:23 -0400

    Arreglado con el cambio de paginas

--- index.js
+++ index.js
@@ -4,32 +4,39 @@ const config = require("./config.json");
 (async () => {
 
   //Instanciando un browser
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({
+    headless: false
+  });
 
   // nueva pag
   const page = await browser.newPage();
 
   // custom viewport
-  await page.setViewport({ width: 1280, height: 800 }) 
+  await page.setViewport({
+    width: 1280,
+    height: 800
+  })
 
   // Metodo para cerrar el navegador
-  let logout = async() => {
+  let logout = async () => {
     console.log("Saliendo");
     await browser.close();
   }
 
   //Metodo login
-  let login = async (username,password) => {
-    
+  let login = async (username, password) => {
+
     console.log("Entrando pagina del itla...")
     // networkidle2 para esperar que cargue todo por completo
-    await page.goto("https://itla.edu.do/virtual/", {waitUntil: 'networkidle2'});
+    await page.goto("https://itla.edu.do/virtual/login/index.php", {
+      waitUntil: 'networkidle2'
+    });
 
 
     // seeh
     console.log("LLenando credenciales...");
-    await page.type("#username",username);
-    await page.type("#password",password);
+    await page.type("#username", username);
+    await page.type("#password", password);
 
     // btn de clickear
     console.log("Logeando...")
@@ -44,35 +51,46 @@ const config = require("./config.json");
   }
 
   //Metodo para obtener todas los enlaces de las asignaturas
-  let asignaturas = async ()=>{
+  let asignaturas = async () => {
     console.log("Buscando materias...")
-    await page.goto("https://itla.edu.do/virtual/?redirect=0",{waitUntil:"networkidle2"});
+    await page.goto("https://itla.edu.do/virtual/?redirect=0", {
+      waitUntil: "networkidle2"
+    });
 
     // esperar que carguen todos los cursos 
-    await page.waitForSelector(".coursebtn");
+    await page.waitForSelector(".coursebox-content");
 
     // solo se necesitan los href 
     console.log("Obteniendo links de materias...");
-    let links = await page.$$eval(".coursebtn", lins => lins.map(a => a.href));
+    let links = await page.$$eval(".coursebox-content a", lins => lins.map(a => a.href));
 
+    links.forEach(element => {
+      if (!(element.match(/user/) == null)) {
+        var index = links.indexOf(element);
+        links.splice(index, 1)
+      }
+    });
     return links;
   }
 
   // Metodo para entrar a cada asignatura
-  let entrarAsig = async(links) => {
+  let entrarAsig = async (links) => {
 
 
     for (let i = 0; i < links.length; i++) {
       let asig = links[i];
 
-      await page.goto(asig, {waitUntil:'networkidle2'});
+      await page.goto(asig, {
+        waitUntil: 'networkidle2'
+      });
 
       // Obtener el nombre de la asignatura ez
       let nombreAsig = await page.$eval('#sitetitle', s => s.innerHTML)
-      console.log("["+nombreAsig+"] Listo!");
+      console.log("[" + nombreAsig + "] Listo!");
 
       // xd
       await page.waitFor(2000);
+
     }
 
   }
